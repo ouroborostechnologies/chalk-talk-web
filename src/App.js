085a6b06ffb64e7cbebc41576a65557d3fd75c9b
Connor Yass <yassck02@gmail.com>
2024-08-05 23:52:56 -0700

    add links

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <img src="/icon.png" width={350} height={350} alt="The Chalk-Talk app logo - a pixelated, winking smiley face." />
-      <a href="https://google.com"><AppStoreBadge /></a>
+    <div className="app">
+      <div className="content">
+        <img src="/icon.png" width={350} height={350} alt="A grey, pixelated, winking smiley face." />
+        <h1>Chalk-Talk</h1>
+        <a href="https://google.com"><AppStoreBadge /></a>
+      </div>
+      <div className="footer">
+        <a href="https://ouroborostechnologies.com">https://ouroborostechnologies.com</a>
+        <a href="">Privacy</a>
+      </div>
     </div>
   );
 }
